40485d86ae1f654ea5e3bab9471e3c816e1ccbf5
Connor Flaat <egfconnor@gmail.com>
2014-08-26 12:23:16 -0400

    Merge branch 'develop' of https://github.com/theonlylawislove/WPFKeyboard into dev

---
+++ Sample/Form.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for Form.xaml
+    /// </summary>
+    public partial class Form
+    {
+        readonly PopupKeyboard _keyboardWindow = new PopupKeyboard();
+
+        public Form()
+        {
+            InitializeComponent();
+            Keyboard.AddGotKeyboardFocusHandler(this, (sender, args) =>
+            {
+                var current = args.NewFocus as FrameworkElement;
+                if (current != null && current.GetType().IsAssignableFrom(typeof(TextBox)))
+                {
+                    _keyboardWindow.Show(current);
+                }
+                else
+                {
+                    _keyboardWindow.Hide();
+                }
+            });
+            IsKeyboardFocusedChanged += OnIsKeyboardFocusedChanged;
+            Deactivated += OnDeactivated;
+        }
+
+        private void OnDeactivated(object sender, EventArgs x)
+        {
+            _keyboardWindow.Hide();
+        }
+
+        private void OnIsKeyboardFocusedChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+        }
+    }
+}
---
+++ Sample/PopupKeyboard.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Sample.KeyTemplates;
+using WPFKeyboard;
+using Point = System.Windows.Point;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for PopupKeyboard.xaml
+    /// </summary>
+    public partial class PopupKeyboard
+    {
+        public PopupKeyboard()
+        {
+            InitializeComponent();
+            VirtualKeyboard.OnScreenKeyControlBuilder = new SampleKeyControlBuilder();
+            VirtualKeyboard.DataContext = new KPDOnScreenKeyboardViewModel(KeyboardHelper.InstalledKeyboardLayouts["US"], new ModiferStateManager());
+        }
+    }
+}
